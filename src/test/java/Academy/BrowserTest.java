b21b8f6995c10fe87a2e2bc08d25c6cb53b8f2c9
Mahesh Deevi <59371127+MaheshDeevi@users.noreply.github.com>
2024-02-15 00:59:09 +0530

    Update BrowserTest.java

--- src/test/java/Academy/BrowserTest.java
+++ src/test/java/Academy/BrowserTest.java
@@ -19,10 +19,10 @@ public class BrowserTest {
 		System.out.println("Hello Guys");
 		System.setProperty("webdriver.chrome.driver", "chromedriver.exe");
 		WebDriver driver =new ChromeDriver();
-		driver.get("https://rahylshettyacademy.azurewebsites.net/webapp/");
-		String text =driver.findElement(By.cssSelector("h1")).getText();
+		driver.get("https://maheshdeevi.azurewebsites.net/webapp/");
+		String text =driver.findElement(By.cssSelector("h1")).getText().trim();
 		System.out.println(text);
-		Assert.assertTrue(text.equalsIgnoreCase("RahulShettyAcademy.com Learning"));
+		Assert.assertTrue(text.equalsIgnoreCase(" Mahesh Deevi welcome's you for azure devops course!!!"));
 		driver.close();
 	
 		
